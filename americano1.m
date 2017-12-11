c60fe57974c4f6bd0695fd197be6d3c72436d087
doerlbh <doerlbh@gmail.com>
2017-12-10 20:16:09 -0500

    update code

---
+++ americano1.m
@@ -0,0 +1,7 @@
+%americano
+
+F_main_classifier(1, 'CIFAR-10', 'nonstationary','shuffled', 1, 'baseline', 1000, 'americano', 20000)
+disp('F_main_classifier(1, CIFAR-10, nonstationary,shuffled, 1,online_embedding, 1000,americano, 20000)')
+
+F_main_classifier(1, 'MNIST-s', 'nonstationary','shuffled', 1, 'baseline', 1000, 'americano', 20000)
+disp('F_main_classifier(1, MNIST-s, nonstationary,shuffled, 1, baseline, 1000, americano, 20000)')
