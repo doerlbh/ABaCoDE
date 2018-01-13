98b7b3312bbf2359edd06add0815b3f445092034
doerlbh <doerlbh@gmail.com>
2018-01-13 17:43:40 -0500

    update code

--- americano1.m
+++
@@ -1,7 +0,0 @@
-%americano
-
-F_main_classifier(1, 'CIFAR-10', 'nonstationary','shuffled', 1, 'baseline', 1000, 'americano', 20000)
-disp('F_main_classifier(1, CIFAR-10, nonstationary,shuffled, 1,online_embedding, 1000,americano, 20000)')
-
-F_main_classifier(1, 'MNIST-s', 'nonstationary','shuffled', 1, 'baseline', 1000, 'americano', 20000)
-disp('F_main_classifier(1, MNIST-s, nonstationary,shuffled, 1, baseline, 1000, americano, 20000)')
