005565aaf589a00f0af95957959ed1251615e646
doerlbh <doerlbh@gmail.com>
2018-01-18 13:05:30 -0500

    update code

---
+++ runwarfarin.m
@@ -0,0 +1,92 @@
+gpuDevice(4)
+
+F_main_classifier_local_2018(1, 'warfarin', 'nonstationary','unshuffled', 0, 'baseline', 1000, 'americano', 5000)
+disp('F_main_classifier_local(1,warfarin, nonstationary, unshuffled, 0, baseline,1000,americano, 5000)')
+system(['echo runwarfarin warfarin unshuffled, 0, baseline >> done'  ]);
+
+F_main_classifier_local_2018(1, 'warfarin', 'nonstationary','unshuffled', 1, 'universal_embedding', 1000, 'americano', 5000)
+disp('F_main_classifier_local(1,warfarin, nonstationary, unshuffled, 1, universal_embedding,1000,americano, 5000)')
+system(['echo runwarfarin warfarin unshuffled, 1, universal_embedding >> done'  ]);
+
+% ==============
+
+F_main_classifier_local_2018(1, 'warfarin', 'nonstationary','unshuffled', 2, 'minibatch_embedding', 1000, 'americano', 5000)
+disp('F_main_classifier_local(1,warfarin, nonstationary, unshuffled, 2, minibatch_embedding,1000,americano, 5000)')
+system(['echo runwarfarin warfarin unshuffled, 2, minibatch_embedding >> done'  ]);
+
+F_main_classifier_local_2018(1, 'warfarin', 'nonstationary','unshuffled', 2, 'online_embedding', 1000, 'americano', 5000)
+disp('F_main_classifier_local(1,warfarin, nonstationary, unshuffled, 2, online_embedding,1000,americano, 5000)')
+system(['echo runwarfarin warfarin unshuffled, 2, online_embedding >> done'  ]);
+
+F_main_classifier_local_2018(1, 'warfarin', 'nonstationary','unshuffled', 2, 'minibatch_history_CB', 1000, 'americano', 5000)
+disp('F_main_classifier_local(1, warfarin, nonstationary,unshuffled, 2, minibatch_history_CB, 1000, americano, 5000)')
+system(['echo runwarfarin warfarin unshuffled, 2, minibatch_history_CB >> done'  ]);
+
+F_main_classifier_local_2018(1, 'warfarin', 'nonstationary','unshuffled', 2, 'full_history_CB', 1000, 'americano', 5000)
+disp('F_main_classifier_local(1, warfarin, nonstationary,unshuffled, 2, full_history_CB, 1000, americano, 5000)')
+system(['echo runwarfarin warfarin unshuffled, 2, full_history_CB >> done'  ]);
+
+F_main_classifier_local_2018(1, 'warfarin', 'nonstationary','unshuffled', 2, 'multimode_minibatch_history_CB', 1000, 'americano', 5000)
+disp('F_main_classifier_local(1, warfarin, nonstationary,unshuffled, 2, multimode_minibatch_history_CB, 1000, americano, 5000)')
+system(['echo runwarfarin warfarin unshuffled, 2, multimode_minibatch_history_CB >> done'  ]);
+
+F_main_classifier_local_2018(1, 'warfarin', 'nonstationary','unshuffled', 2, 'multimode_full_history_CB', 1000, 'americano', 5000)
+disp('F_main_classifier_local(1, warfarin, nonstationary,unshuffled, 2, multimode_full_history_CB, 1000, americano, 5000)')
+system(['echo runwarfarin warfarin unshuffled, 2, multimode_full_history_CB >> done'  ]);
+
+% ==============
+
+F_main_classifier_local_2018(1, 'warfarin', 'nonstationary','unshuffled', 4, 'minibatch_embedding', 1000, 'americano', 5000)
+disp('F_main_classifier_local(1,warfarin, nonstationary, unshuffled, 4, minibatch_embedding,1000,americano, 5000)')
+system(['echo runwarfarin warfarin unshuffled, 4, minibatch_embedding >> done'  ]);
+
+F_main_classifier_local_2018(1, 'warfarin', 'nonstationary','unshuffled', 4, 'online_embedding', 1000, 'americano', 5000)
+disp('F_main_classifier_local(1,warfarin, nonstationary, unshuffled, 4, online_embedding,1000,americano, 5000)')
+system(['echo runwarfarin warfarin unshuffled, 4, online_embedding >> done'  ]);
+
+F_main_classifier_local_2018(1, 'warfarin', 'nonstationary','unshuffled', 4, 'minibatch_history_CB', 1000, 'americano', 5000)
+disp('F_main_classifier_local(1, warfarin, nonstationary,unshuffled, 4, minibatch_history_CB, 1000, americano, 5000)')
+system(['echo runwarfarin warfarin unshuffled, 4, minibatch_history_CB >> done'  ]);
+
+F_main_classifier_local_2018(1, 'warfarin', 'nonstationary','unshuffled', 4, 'full_history_CB', 1000, 'americano', 5000)
+disp('F_main_classifier_local(1, warfarin, nonstationary,unshuffled, 4, full_history_CB, 1000, americano, 5000)')
+system(['echo runwarfarin warfarin unshuffled, 4, full_history_CB >> done'  ]);
+
+F_main_classifier_local_2018(1, 'warfarin', 'nonstationary','unshuffled', 4, 'multimode_minibatch_history_CB', 1000, 'americano', 5000)
+disp('F_main_classifier_local(1, warfarin, nonstationary,unshuffled, 4, multimode_minibatch_history_CB, 1000, americano, 5000)')
+system(['echo runwarfarin warfarin unshuffled, 4, multimode_minibatch_history_CB >> done'  ]);
+
+F_main_classifier_local_2018(1, 'warfarin', 'nonstationary','unshuffled', 4, 'multimode_full_history_CB', 1000, 'americano', 5000)
+disp('F_main_classifier_local(1, warfarin, nonstationary,unshuffled, 4, multimode_full_history_CB, 1000, americano, 5000)')
+system(['echo runwarfarin warfarin unshuffled, 4, multimode_full_history_CB >> done'  ]);
+
+% ==============
+
+F_main_classifier_local_2018(1, 'warfarin', 'nonstationary','unshuffled', 8, 'minibatch_embedding', 1000, 'americano', 5000)
+disp('F_main_classifier_local(1,warfarin, nonstationary, unshuffled, 8, minibatch_embedding,1000,americano, 5000)')
+system(['echo runwarfarin warfarin unshuffled, 8, minibatch_embedding >> done'  ]);
+
+F_main_classifier_local_2018(1, 'warfarin', 'nonstationary','unshuffled', 8, 'online_embedding', 1000, 'americano', 5000)
+disp('F_main_classifier_local(1,warfarin, nonstationary, unshuffled, 8, online_embedding,1000,americano, 5000)')
+system(['echo runwarfarin warfarin unshuffled, 8, online_embedding >> done'  ]);
+
+F_main_classifier_local_2018(1, 'warfarin', 'nonstationary','unshuffled', 8, 'minibatch_history_CB', 1000, 'americano', 5000)
+disp('F_main_classifier_local(1, warfarin, nonstationary,unshuffled, 8, minibatch_history_CB, 1000, americano, 5000)')
+system(['echo runwarfarin warfarin unshuffled, 8, minibatch_history_CB >> done'  ]);
+
+F_main_classifier_local_2018(1, 'warfarin', 'nonstationary','unshuffled', 8, 'full_history_CB', 1000, 'americano', 5000)
+disp('F_main_classifier_local(1, warfarin, nonstationary,unshuffled, 8, full_history_CB, 1000, americano, 5000)')
+system(['echo runwarfarin warfarin unshuffled, 8, full_history_CB >> done'  ]);
+
+F_main_classifier_local_2018(1, 'warfarin', 'nonstationary','unshuffled', 8, 'multimode_minibatch_history_CB', 1000, 'americano', 5000)
+disp('F_main_classifier_local(1, warfarin, nonstationary,unshuffled, 8, multimode_minibatch_history_CB, 1000, americano, 5000)')
+system(['echo runwarfarin warfarin unshuffled, 8, multimode_minibatch_history_CB >> done'  ]);
+
+F_main_classifier_local_2018(1, 'warfarin', 'nonstationary','unshuffled', 8, 'multimode_full_history_CB', 1000, 'americano', 5000)
+disp('F_main_classifier_local(1, warfarin, nonstationary,unshuffled, 8, multimode_full_history_CB, 1000, americano, 5000)')
+system(['echo runwarfarin warfarin unshuffled, 8, multimode_full_history_CB >> done'  ]);
+
+exit
+
+
+
